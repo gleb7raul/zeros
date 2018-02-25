2601d5f923f70f92819868a6cfc7f6bd41efffa3
gleb7raul <glebimptab@gmail.com>
2018-02-25 16:48:15 +0200

    Zeros done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let temp =  0;
+  let result = 0;
+  temp = Math.floor(number / 5);
+  result += temp;
+  temp = Math.floor(number / 25);
+  result += temp;
+  temp = Math.floor(number / 125);
+  result += temp;
+  temp = Math.floor(number / 625);
+  result += temp;
+  temp = Math.floor(number / 3125);
+  result += temp;
+  temp = Math.floor(number / 15625);
+  result += temp;
+  temp = Math.floor(number / 78125);
+  result += temp;
+  temp = Math.floor(number / 390625);
+  result += temp;
+  temp = Math.floor(number / 1953125);
+  result += temp;
+  temp = Math.floor(number / 9765625);
+  result += temp;
+  if (number >= 48828125) {
+      temp = Math.floor(number / 48828125);
+      result += temp;
+  }
+return(result);
 }
